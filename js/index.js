7c3d1f2843962edf43a8183f180c14f0de623417
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-01-31 20:27:23 +0100

    Finished styling, started functionality

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const app = document.querySelector('.app');
+const tweetTextArea = app.querySelector('#tweetTextArea');
+const counter = app.querySelector('#counter');
+const tweetBtn = app.querySelector('#tweetBtn');
+const maxChars = 140;   
